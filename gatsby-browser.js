38172db60fc0857186f0a1e52fb9254b81827db0
nisten <nisten@outlook.com>
2019-04-01 23:23:22 -0400

    dicom init

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,10 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
+export const onServiceWorkerUpdateReady = () => {
+    const answer = window.confirm(
+      `App has been updated!` +
+        `Reload?`
+    )
+  
+    if (answer === true) {
+      window.location.reload()
+    }
+  }
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,14 +3,19 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import DwvComponent from './DwvComponent'
 
 const SecondPage = () => (
+  
   <Layout>
+    <DwvComponent />
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
+    
     <Link to="/">Go back to the homepage</Link>
   </Layout>
+  
 )
 
 export default SecondPage
